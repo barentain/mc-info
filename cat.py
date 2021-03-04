a88790e688b4a30d5f18addca671c672b967e518
valentine <v@taehyung.org>
2021-03-03 20:19:45 -0500

    finished

--- cat.py
+++ cat.py
@@ -1,6 +1,34 @@
 import requests, json, logging
 
-# i have decided to finish coding this later because i did not have my coffee
+minecraft_name = input("[!] Enter a MC username: ")
 
-def req(url, name):
-	resp = requests.get("")
+def converttostr(input_seq, seperator):
+   # Join all the strings in list
+   final_str = seperator.join(input_seq)
+   return final_str
+
+def req(name):
+	try:
+		resp = requests.get("https://api.ashcon.app/mojang/v2/user/" + name)
+		data = json.loads(resp.text)
+
+		print("[*] Username: " + data["username"])
+		print("[*] UUID: " + data["uuid"])
+		if data["created_at"] == None:
+			print("[*] Created: Not Found")
+		else:
+			print("[*] Created: " + data["created_at"])
+		print("\n[*] Name History: ")
+
+		# Checking for username history
+		for names in data["username_history"]:
+			try:
+				print(" - " + names["username"] + f"(Changed {data['changed_at']})")
+			except:
+				print(" - " + names["username"])
+	
+	except Exception as x:
+		logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+		logging.critical(x)
+
+req(minecraft_name)
